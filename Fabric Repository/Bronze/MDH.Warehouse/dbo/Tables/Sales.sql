52a04a3bd3d2f7d28a3f9c6f0daa8c31dcc8e966
srinivask8 <108177561+srinivask8@users.noreply.github.com>
2026-04-28 21:01:05 +0100

    Committing 5 items from workspace 289b01ef-d1dd-4f5c-9b7c-18b7b4bcf9e9

---
+++ Fabric Repository/Bronze/MDH.Warehouse/dbo/Tables/Sales.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Sales] (
+
+	[SaleID] int NULL, 
+	[CustomerID] int NULL, 
+	[ProductID] int NULL, 
+	[Quantity] int NULL, 
+	[SaleDate] date NULL, 
+	[TotalAmount] float NULL
+);
